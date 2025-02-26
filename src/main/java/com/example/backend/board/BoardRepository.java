905514e98f83dea4c5c25aa57134e8a8ab103f13
seiyeon <wsx4379@naver.com>
2025-02-26 14:10:56 +0900

    [Fix] Board Dto 수정 (댓글 수 추가)
    Board Dto 댓글 수 나오게 수정

--- src/main/java/com/example/backend/board/BoardRepository.java
+++ src/main/java/com/example/backend/board/BoardRepository.java
@@ -1,7 +1,10 @@
 package com.example.backend.board;
 
 import com.example.backend.board.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAllByOrderByIdxDesc(PageRequest pageRequest);
 }
